852d2107e6c6bab4768a98ca9e228016822b8071
Matthew Jones <mail@matthewjones.io>
2019-08-25 09:19:56 +0100

    Indentation fixes

--- Plugins/EggEmAll/src/main/java/com/shadowblockz/eggemall/database/Database.java
+++ Plugins/EggEmAll/src/main/java/com/shadowblockz/eggemall/database/Database.java
@@ -7,51 +7,51 @@ import java.sql.Statement;
 
 public abstract class Database 
 {
-	protected Connection connection;
+    protected Connection connection;
 
-	protected Database() 
-	{
-		this.connection = null;
-	}
+    protected Database()
+    {
+        this.connection = null;
+    }
 
-	public abstract Connection openConnection() throws SQLException, ClassNotFoundException;
+    public abstract Connection openConnection() throws SQLException, ClassNotFoundException;
 
-	public boolean checkConnection() throws SQLException
-	{
-		return connection != null && !connection.isClosed();
-	}
+    public boolean checkConnection() throws SQLException
+    {
+        return connection != null && !connection.isClosed();
+    }
 
-	public Connection getConnection() 
-	{
-		return connection;
-	}
+    public Connection getConnection()
+    {
+        return connection;
+    }
 
-	public boolean closeConnection() throws SQLException 
-	{
-		if (connection == null)
-			return false;
-		
-		connection.close();
-		return true;
-	}
+    public boolean closeConnection() throws SQLException
+    {
+        if (connection == null)
+            return false;
 
-	public ResultSet querySQL(String query) throws SQLException, ClassNotFoundException 
-	{
-		if (!checkConnection())
-			openConnection();
+        connection.close();
+        return true;
+    }
 
-		Statement statement = connection.createStatement();
-		ResultSet result = statement.executeQuery(query);
-		return result;
-	}
+    public ResultSet querySQL(String query) throws SQLException, ClassNotFoundException
+    {
+        if (!checkConnection())
+            openConnection();
 
-	public int updateSQL(String query) throws SQLException, ClassNotFoundException 
-	{
-		if (!checkConnection())
-			openConnection();
+        Statement statement = connection.createStatement();
+        ResultSet result = statement.executeQuery(query);
+        return result;
+    }
 
-		Statement statement = connection.createStatement();
-		int result = statement.executeUpdate(query);
-		return result;
-	}
+    public int updateSQL(String query) throws SQLException, ClassNotFoundException
+    {
+        if (!checkConnection())
+            openConnection();
+
+        Statement statement = connection.createStatement();
+        int result = statement.executeUpdate(query);
+        return result;
+    }
 }
--- Plugins/EggEmAll/src/main/java/com/shadowblockz/eggemall/events/Commands.java
+++ Plugins/EggEmAll/src/main/java/com/shadowblockz/eggemall/events/Commands.java
@@ -9,33 +9,33 @@ import org.bukkit.event.Listener;
 
 public class Commands implements CommandExecutor, Listener
 {
-	/**
-	 * Handles player commands.
-	 * @param sender the player whom sent it.
-	 * @param cmd the command itself.
-	 * @param label the label.
-	 * @param args the command arguments.
-	 * @return true if the command was processed successfully.
-	 */
+    /**
+     * Handles player commands.
+     * @param sender the player whom sent it.
+     * @param cmd the command itself.
+     * @param label the label.
+     * @param args the command arguments.
+     * @return true if the command was processed successfully.
+     */
     @Override
-   	public boolean onCommand(CommandSender sender, org.bukkit.command.Command cmd, String label, String[] args)
-	{
-    	if(sender instanceof Player)
-		{
-    		if(cmd.getName().equalsIgnoreCase("queue"))
-			{
-  		  		Player player = (Player) sender;
-  		  		player.sendMessage(ChatColor.YELLOW + "[EEA] " + ChatColor.RESET + "Your position in the queue is currently" + (Main.gameManager.GetQueue().indexOf(player) + 1));
+    public boolean onCommand(CommandSender sender, org.bukkit.command.Command cmd, String label, String[] args)
+    {
+        if(sender instanceof Player)
+        {
+            if(cmd.getName().equalsIgnoreCase("queue"))
+            {
+                Player player = (Player) sender;
+                player.sendMessage(ChatColor.YELLOW + "[EEA] " + ChatColor.RESET + "Your position in the queue is currently" + (Main.gameManager.GetQueue().indexOf(player) + 1));
 
-	  			if((Main.gameManager.GetQueue().indexOf(player) + 1) > 8)
-    				player.sendMessage(ChatColor.YELLOW + "[EEA] " + ChatColor.RESET + "You will most likley be spectating next round.");
-				else 
-    				player.sendMessage(ChatColor.YELLOW + "[EEA] " + ChatColor.RESET + "You will most likley be playing next round.");
-    		 }
-    	 }
-		 else
-		 	return false; // Error, has to be a player.
+                if((Main.gameManager.GetQueue().indexOf(player) + 1) > 8)
+                    player.sendMessage(ChatColor.YELLOW + "[EEA] " + ChatColor.RESET + "You will most likley be spectating next round.");
+                else
+                    player.sendMessage(ChatColor.YELLOW + "[EEA] " + ChatColor.RESET + "You will most likley be playing next round.");
+             }
+         }
+         else
+            return false; // Error, has to be a player.
 
-		return true; 	
-	}
+        return true;
+    }
 }
